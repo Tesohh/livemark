9e5a1af66c1fc11d5d66a1e5d93791bac59bc903
Simone Tesini <simozilla006@gmail.com>
2023-01-11 18:32:44 +0100

    aaaa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,15 @@
 	<Link href="https://github.com/Tesohh/livemark">GitHub</Link>
 	<Link href="about">About</Link>
 </Nav>
+<div class="absolute top-0 min-h-screen ml-8 flex items-center">
+	<div class="flex flex-col">
+		<span class="text-6xl font-bold">
+			<span class="text-theo-400 dark:text-theo-700">#</span> Livemark</span
+		>
+		<span class="text-2xl"
+			>the <span class="text-theo-400 dark:text-theo-700">__</span><span class="italic"
+				>markdown</span
+			><span class="text-theo-400 dark:text-theo-700">__</span> editor that reacts</span
+		>
+	</div>
+</div>
